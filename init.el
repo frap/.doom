b24524b312e2d960ac2c9034a2060b82fc68df4e
Andrés Gasson <frap@red-elvis.net>
2021-12-15 10:41:03 +1300

    removed vertico

--- init.el
+++ init.el
@@ -20,14 +20,14 @@
 
        :completion
        (:if IS-MAC
-       (company +auto +childframe) ; the ultimate code completion backend
+       (company +auto) ;; +childframe) ; the ultimate code completion backend
        (company +tng))       ; tng is tab completion
        ;helm              ; the *other* search engine for love and life
-       ;; (:if IS-MAC
-       ;;   (ivy +prescient +icons -childframe -fuzzy)
-       ;;   (ivy +prescient +icons))               ; a search engine for love and life
-       ;;ido
-       (vertico +icons)             ; the search engine of the future
+       (:if IS-MAC
+          (ivy +prescient +icons ) ;;-childframe -fuzzy)
+          (ivy +prescient +icons))               ; a search engine for love and life
+       ido
+       ;;(vertico +icons)             ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
